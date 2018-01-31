b997dfd3874ab1c7320c7ffd490b3f1ad5fc8125
Shirley Zhang <shirleyz9402@yahoo.com>
2018-01-31 23:39:22 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -2,8 +2,8 @@ def get_first_name_of_season_winner(data, season)
   data.each do |se, people|
     if se = season 
       people.each do |details|
-        if details[:status] == "Winner"
-          return details[:name].split(" ")[0]
+        if details["status"] == "Winner"
+          return details["name"].split(" ")[0]
         end 
       end
     end 
