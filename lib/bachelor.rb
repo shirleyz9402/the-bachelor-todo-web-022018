d845fdf0afc1621c8942cc34847a7599a2ee6a8b
Shirley Zhang <shirleyz9402@yahoo.com>
2018-02-01 06:03:23 +0000

    automatically backed up by learn

--- lib/bachelor.rb
+++ lib/bachelor.rb
@@ -47,7 +47,7 @@ def get_average_age_for_season(data, season)
    data.each do |se, people|
      if se == season
       people.each do |details|
-        ages << details["age".to_s.to_i]
+        ages << details["age"].to_i
       end 
     end 
     end 
